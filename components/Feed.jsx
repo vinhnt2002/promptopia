b0eb76ddd867d45a9b90549549eaac3f1797f936
vinh nguyễn <vinhntse2002@gmail.com>
2023-06-24 12:18:08 +0700

    search function

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,27 +1,42 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import PromptCard from "./PromptCard";
-
-const PromptCardList = ({ data, handleTagClick }) => {
-  return (
-    <div className="mt-16 prompt_layout">
-      {data.map((post) => (
-        <PromptCard
-        key={post._id}
-        post={post}
-        handleTagClick={handleTagClick}
-        />
-      ))}
-    </div>
-  );
-};
+import PromptCardList from "./PromptCardList";
 
 const Feed = () => {
-  const [searchText, setSearchText] = useState("");
   const [posts, setPosts] = useState([]);
 
-  const handleSearchChange = (e) => {};
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchResults, setSearchResults] = useState([]);
+
+  const filterPrompts = (searchText) => {
+    const regex = new RegExp(searchText, "i");
+    return posts.filter(
+      (post) => regex.test(post.creator.username) || regex.test(post.tag) || regex.test(post.prompt)
+    );
+  };
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    //debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchResults(searchResult);
+      }),
+      500
+    );
+  };
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchResults(searchResult);
+  };
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -47,8 +62,14 @@ const Feed = () => {
             className="search_input peer"
           />
         </form>
-
-        <PromptCardList data={posts} handleTagClick={() => {}} />
+        {searchText ? (
+          <PromptCardList
+            data={searchResults}
+            handleTagClick={handleTagClick}
+          />
+        ) : (
+          <PromptCardList data={posts} handleTagClick={handleTagClick} />
+        )}
       </section>
     </div>
   );
